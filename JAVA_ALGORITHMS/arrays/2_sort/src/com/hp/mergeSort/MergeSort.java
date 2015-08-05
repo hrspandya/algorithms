72fd1c2d082554a296aed95f6bf765844854667c
Harsh Pandya <hrspandya@gmail.com>
2015-08-04 20:06:07 -0700

    Merge Sort

--- JAVA_ALGORITHMS/arrays/2_sort/src/com/hp/mergeSort/MergeSort.java
+++ JAVA_ALGORITHMS/arrays/2_sort/src/com/hp/mergeSort/MergeSort.java
@@ -30,27 +30,34 @@ public class MergeSort {
 
 	private long[] recMergeSort(long[] array) {
 		int n = array.length;
+		//If Array has no elements or only 1 element then return the array as result
 		if(n <= 1){
 			return array;
 		}else{
+			
+			//Create 2 arrays Left and right and divide them into two
 			int mid = n/2;
 			long[] leftArray = new long[mid];
 			long[] rightArray = new long[n - mid];
 			long[] resultArray = new long[n];
 			
 			
+			//populate Left array
 			for(int i = 0; i < mid; i++){
 				leftArray[i] = array[i];
 			}
 			
+			//populate right array
 			for(int j = mid, i = 0; j < n; j++){				
 				rightArray[i] = array[j];
 				i++;
 			}
 			
+			//recursion 
 			leftArray = recMergeSort(leftArray);
 			rightArray = recMergeSort(rightArray);
 			
+			//merge left and right array, and return result array
 			resultArray = merge(leftArray, rightArray);
 			return resultArray;
 		}
@@ -65,7 +72,10 @@ public class MergeSort {
 		int resultIndex = 0;
 		long[] mergedArray = new long[leftArray.length + rightArray.length];
 		
+		//Check if left array and right array are not empty
 		while(leftIndex < leftArray.length && rightIndex < rightArray.length){
+			
+			//start adding smaller numbers to the result array and increment their indexes
 			if(leftArray[leftIndex] < rightArray[rightIndex]){
 				mergedArray[resultIndex] = leftArray[leftIndex];
 				resultIndex++;
@@ -77,12 +87,14 @@ public class MergeSort {
 			}
 		}
 		
+		//check if left array is emptry
 		while(leftIndex < leftArray.length){
 			mergedArray[resultIndex] = leftArray[leftIndex];
 			resultIndex++;
 			leftIndex++;
 		}
 		
+		//check if right array is empty
 		while(rightIndex < rightArray.length){
 			mergedArray[resultIndex] = rightArray[rightIndex];
 			resultIndex++;
