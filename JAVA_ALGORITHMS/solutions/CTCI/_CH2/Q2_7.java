65244a4fd10debb5761d1c9771d43edacbaeb153
Harsh Pandya <hrspandya@gmail.com>
2016-11-17 23:13:30 -0800

    CTCI Ch2 Q7

--- JAVA_ALGORITHMS/solutions/CTCI/_CH2/Q2_7.java
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH2/Q2_7.java
@@ -1,22 +1,25 @@
 package _CH2;
 
-import com.hp.linklist.LinkList;
+import java.util.LinkedList;
 
 //Question :  Implement a function to check if a linked list is a palindrome
-
+//example : 121 == 121 , cac = cac
 
 public class Q2_7 {
 	
 	//Solution 1: palindrome means if you reverse the string its same as original string.
 	//so first we loop in the linked list and create string then we revert that string and match with original and decide.
-	
+	//example : 121 == 121 
 	
 	
 	
 	public static void main(String[] args) {
-		LinkList list = new LinkList();
+		LinkedList<String> list = new LinkedList<>();
+		list.add("c");
+		list.add("a");
+		list.add("c");
 		
-		
+		//System.out.println(isPalindrome(list));
 
 	}
 
