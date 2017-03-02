cf64f471420d155bae8b5ccbf9bce6b487871aec
Harsh Pandya <hrspandya@gmail.com>
2017-03-01 19:49:22 -0800

    Q3_3

---
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH3_3/Q3_3.java
@@ -0,0 +1,19 @@
+package _CH3_3;
+
+/*
+ * Question : Imagine a (literal) stack of plates. If the stack gets too high, it migh t topple. 
+ * Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. 
+ * Implement a data structure SetOfStacks that mimics this. SetOf- Stacks should be composed of several stacks 
+ * and should create a new stack once the previous one exceeds capacity. 
+ * SetOfStacks.push() and SetOfStacks. pop () should behave identically to a single stack 
+ * (that is, pop () should return the same values as it would if there were just a single stack).
+ * 
+ * FOLLOW UP
+ * Implement a function popAt(int index) which performs apop operation ona specific sub-stack 
+ */
+
+//Answer:   Just look at the solution its very easy to implement
+
+public class Q3_3 {
+
+}
