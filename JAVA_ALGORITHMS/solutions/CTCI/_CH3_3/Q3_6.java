7354d56a6e0ef34daae6261f99239ca527129db0
Harsh Pandya <hrspandya@gmail.com>
2017-03-01 19:49:53 -0800

    Q3_6

---
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH3_3/Q3_6.java
@@ -0,0 +1,19 @@
+package _CH3_3;
+
+public class Q3_6 {
+	
+	/*
+	 * Question: Write a program to sort a stack in ascending order (with biggest items on top). 
+	 * You may use at most one additional stack to hold items, 
+	 * but you may not copy the elements into any other data structure (such as an array). 
+	 * The stack supports the following operations: push, pop, peek, and isEmpty. 
+	 */
+	
+	//Solution:  read the solution its very easy...
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
