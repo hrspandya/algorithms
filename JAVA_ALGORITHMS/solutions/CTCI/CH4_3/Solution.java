f0abdd76d66b09135c6b4241f8152e18299b410b
Harsh Pandya <hrspandya@gmail.com>
2017-03-07 21:30:37 -0800

    CH4_3

---
+++ JAVA_ALGORITHMS/solutions/CTCI/CH4_3/Solution.java
@@ -0,0 +1,17 @@
+package CH4_3;
+
+import com.hp.a_binaryTree.Node;
+import com.hp.a_binaryTree.Tree;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		Tree tree = new Tree();
+		Node node = tree.createMinimalBST(array);
+		
+	}
+
+}
