48c88ab1d99b47f9e84cd5c52efba96109515e9a
Harsh Pandya <hrspandya@gmail.com>
2017-03-09 19:53:42 -0800

    no message

---
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH4/CH4_5/Solution.java
@@ -0,0 +1,26 @@
+package _CH4.CH4_5;
+
+import com.hp.a_binaryTree.Tree;
+
+public class Solution {
+
+	
+	public static void main(String[] args) {
+		
+		Tree mytree = new Tree();
+		
+		mytree.insert(4, 4);
+		mytree.insert(2, 2);
+		mytree.insert(3, 3);
+		mytree.insert(1, 1);
+		mytree.insert(6, 6);
+		mytree.insert(9, 9);
+		mytree.insert(8, 8);
+		mytree.insert(5, 5);
+		mytree.insert(7, 7);
+					
+		System.out.println( "is BST : " + mytree.isBST());
+	}
+	
+	
+}
