c4bbc1a80e10415afc53d5d4f88fdeef8ef91f92
Harsh Pandya <hrspandya@gmail.com>
2017-03-14 17:03:41 -0700

    no message

--- JAVA_ALGORITHMS/solutions/CTCI/_CH4/CH4_7/Solution_1.java
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH4/CH4_7/Solution_1.java
@@ -1,9 +1,24 @@
 package _CH4.CH4_7;
 
 public class Solution_1 {
-
+	
+	/*
+	 * Question: Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree. 
+	 * Avoid storing additional nodes in a data structure. 
+	 * NOTE: This is not necessarily a binary search tree.
+	 */
+	
+	
+	/*
+	 * Best Solution will be is to keep going to parent until you reach root for one node and for 
+	 * every node set isVisited = true; Now from another Node keep going to parent until you see 
+	 * node which has isVisited = true and you have find an ancestor. 
+	 */
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
