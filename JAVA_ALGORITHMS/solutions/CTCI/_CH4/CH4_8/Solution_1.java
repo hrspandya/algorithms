3f6a98ac49b2fde4daba2c8c9c9925ca1fae9392
Harsh Pandya <hrspandya@gmail.com>
2017-03-22 17:32:57 -0700

    no message

---
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH4/CH4_8/Solution_1.java
@@ -0,0 +1,23 @@
+package _CH4.CH4_8;
+
+public class Solution_1 {
+	
+	/*
+	 * Question : You have two very large binary trees: Tl, with millions of nodes, and T2, with
+	 * hundreds of nodes. Create an algorithm to decideifT2 isa subtree ofTl.
+	 * A tree T2 is a subtree of Tl if there exists a node n in Tl such that the subtree ofn is identical to T2. 
+	 * That is, if you cut off the tree at node n, the two trees would be identical.
+	 */
+	
+	
+	/*
+	 * Answer:  
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
