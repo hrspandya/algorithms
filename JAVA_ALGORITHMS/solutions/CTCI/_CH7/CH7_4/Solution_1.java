993023a59bb493f82e29815e6129de96f7bf252a
Harsh Pandya <hrspandya@gmail.com>
2017-04-20 17:54:01 -0700

    no message

---
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH7/CH7_4/Solution_1.java
@@ -0,0 +1,45 @@
+package _CH7.CH7_4;
+
+public class Solution_1 {
+	
+	/*
+	 * Question: Write methods to implement the multiply, subtract, and divide operations for integers. 
+	 * Use only the add operator.
+	 */
+	
+	public static void main(String[] args) {		
+		System.out.println("Multiply : ");
+		System.out.println(multiply(2, 10));
+		System.out.println(multiply(100, 4));
+		System.out.println(multiply(36, 5));
+		
+		
+		
+		System.out.println("Subtract : ");
+		System.out.println(multiply(2, 10));
+		System.out.println(multiply(2, -10));
+		System.out.println(multiply(-10, 5));
+		System.out.println(multiply(-10, -5));
+		
+		
+		
+		System.out.println("Devide : ");
+		System.out.println(multiply(2, 10));
+		System.out.println(multiply(100, 4));
+		System.out.println(multiply(36, 5));
+		
+		
+	}
+	
+	public static int multiply(int a, int b){
+		
+		return 0;
+	}
+	
+	
+	
+	
+	
+	
+
+}
