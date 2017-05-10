d1f8ee83fa19bce1e43f187c2e119c275620032f
Harsh Pandya <hrspandya@gmail.com>
2017-05-10 08:21:28 -0700

    no message

---
+++ JAVA_ALGORITHMS/solutions/leetcode/src/_2_25_reverse_nodes/Solution_1.java
@@ -0,0 +1,24 @@
+package _2_25_reverse_nodes;
+
+public class Solution_1 {
+
+	/*
+    if k = 4, 
+    
+    while(k < 0){
+        while(go from 0 to k){
+            swap swap swap and put 1 where 4 was
+            then 
+            second iteration 
+            swap swap and put 2 where 3 was 
+            third iteration 
+            swap and put 3 where 2 was
+            and 4 will be where 1 was.
+            done.
+        }
+        k--;
+    } 
+    
+    */
+	
+}
