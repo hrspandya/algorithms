2e8c7cbfb00cf73d7bc125d8b6bdf6860590a59a
Harsh Pandya <hrspandya@gmail.com>
2017-05-12 20:14:06 -0700

    no message

---
+++ JAVA_ALGORITHMS/solutions/leetcode/src/_2_27_remove_element/Solution_1.java
@@ -0,0 +1,41 @@
+package _2_27_remove_element;
+
+public class Solution_1 {
+	
+	/*
+	 * Given an array and a value, remove all instances of that value in place and return the new length.
+		
+		Do not allocate extra space for another array, you must do this in place with constant memory.
+		
+		The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+		
+		Example:
+		Given input array nums = [3,2,2,3], val = 3
+		
+		Your function should return length = 2, with the first two elements of nums being 2.
+	 */
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public int removeElement(int[] nums, int val) {
+        if(nums.length < 1){
+            return 0;
+        }
+        
+        int id = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[id] = nums[i];
+                id = id + 1;
+            }else{
+                //do nothing
+            }
+        }
+        
+        return id;
+    }
+}
