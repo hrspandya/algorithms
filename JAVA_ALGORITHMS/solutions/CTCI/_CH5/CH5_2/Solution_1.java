f60fa81c79d506edb5d7feb2a2eaeaaf4532add9
Harsh Pandya <hrspandya@gmail.com>
2017-07-23 11:59:22 -0700

    no message

--- JAVA_ALGORITHMS/solutions/CTCI/_CH5/CH5_2/Solution_1.java
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH5/CH5_2/Solution_1.java
@@ -3,7 +3,7 @@ package _CH5.CH5_2;
 public class Solution_1 {
 	
 	/*
-	 * Question:Given a real number between 0 and 7 (e.g., 0.72) that is passed in as a double, 
+	 * Question:Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, 
 	 * print the binary representation. If the number cannot be represented accurately in binary with at most 32 characters, 
 	 * print "ERROR."
 	 */
