dbf1491beb059b31c77aa55a3ef3059bfabb58a5
Harsh Pandya <hrspandya@gmail.com>
2017-08-09 07:33:06 -0700

    no message

--- JAVA_ALGORITHMS/solutions/CTCI/_CH5/CH5_6/Solution_1.java
+++ JAVA_ALGORITHMS/solutions/CTCI/_CH5/CH5_6/Solution_1.java
@@ -6,7 +6,7 @@ public class Solution_1 {
 	
 	/*
 	 * Question : Write a program to swap odd and even bits in an integer with as few instructions as possible 
-	 * (e.g., bit 0 and bit! are swapped, bit 2 and bit 3 are swapped, and so on).
+	 * (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on).
 	 */
 	
 	
